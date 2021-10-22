6848ab2cf95aa3f1bdae839785f421db26192ea1
michaeleymann <m.eymann@hotmail.ch>
2021-10-22 17:37:07 +0200

    Stop Drag in JS

--- sketch.js
+++ sketch.js
@@ -10,6 +10,11 @@ Check https://sequelaencollection.home.blog/2d-chaotic-attractors/
 Von dort ist auch der grösste Teil des Codes.
 */
 
+function preventBehaviour(e){
+  e.preventDefault();
+}
+document.addEventListener("touchmove",preventBehaviour, {passive: false});
+
 //Variabeln für den Attractor
 let a=0.5;
 var b=1.56;
